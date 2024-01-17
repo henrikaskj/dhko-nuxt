56267fe5d0cd3d5e4c7a72fbb96b1c85f3f75168
Henrik.Askjer <henrik.askjer@uib.no>
2024-01-17 21:13:38 +0100

    tweak styling

--- app.vue
+++ app.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="min-h-screen flex flex-col" :key="i18n.locale">
 
-    <header class="p-10 bg-gradient-to-tr bg-slate-100 shadow-md flex flex-col justify-center items-center">
-      <nav class="absolute top-0 right-0 p-4">
-        <NuxtLink v-if="$i18n.locale != 'en'" lang="en" class="text-teal-700 no-underline flex items-center gap-1" :to="switchLocalePath('en')" @click="setLocale('en')"><LocaleIcon/>English</NuxtLink>
-        <NuxtLink v-if="$i18n.locale != 'no'" lang="no" class="text-teal-700 no-underline flex items-center gap-1" :to="switchLocalePath('no')" @click="setLocale('no')"><LocaleIcon/>Norsk</NuxtLink> 
+    <header class="p-10 bg-gradient-to-tr bg-slate-700 shadow-sm  flex flex-col justify-center items-center">
+      <nav class="absolute top-0 right-2 p-4">
+        <NuxtLink v-if="$i18n.locale != 'en'" lang="en" class="text-slate-50 no-underline flex items-center gap-1" :to="switchLocalePath('en')"><LocaleIcon/>English</NuxtLink>
+        <NuxtLink v-if="$i18n.locale != 'no'" lang="no" class="text-slate-50 no-underline flex items-center gap-1" :to="switchLocalePath('no')"><LocaleIcon/>Norsk</NuxtLink> 
       </nav>
-      <h1 class="font-bold text-3xl text-teal-700 mb-2">DHKO</h1>
-      <h2 class="text-xl text-teal-900">{{$t('description')}}</h2>
-            <NavBar :key="i18n.locale"/>
+      <h1 class="font-bold text-3xl lg:text-4xl text-slate-50   mb-2">DHKO</h1>
+      <h2 class="text-xl lg:text-2xl text-slate-50">{{$t('description')}}</h2>
+            <div class="mt-5 min-h-[2rem]">
+            <NavBar :key="i18n.locale.value"/>
+            </div>
     </header>
     <main class="container mx-auto p-10">
     <NuxtPage/>
     </main>
-    <footer class="p-5 bg-amber-100 shadow-md">
+    <footer class="p-5 bg-slate-100 text-center">
       <p class="text-slate-800">&copy; 2022 DHKO</p>
     </footer>
   </div>
